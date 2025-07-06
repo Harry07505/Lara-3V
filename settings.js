c51a592d73533586dab38825c1066174043c372e
Ryuk.dzz <142965013+Harry07505@users.noreply.github.com>
2025-07-06 16:55:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Add_Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "qIZTUaxT#33m1ZF7reraK6fQB5J8YqQSxAxD1vFwUbOtyfdOKU90", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94717217976",
